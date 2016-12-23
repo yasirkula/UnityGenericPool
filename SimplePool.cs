6b1271c9641d7d344c6f5dd141f0baca7ddaaea8
Süleyman Yasir KULA <yasirkula@yahoo.com>
2016-12-23 22:04:15 +0200

    Simplified the pool, added helper class (see README)
    - Removed the code that automatically deactivates and repositions the pooled objects since this behaviour can manually be set on OnPush event of the pool for the desired pools
    - Added SimplePoolHelper to easily access pools from anywhere in your scripts
    - Added bulk Push/Pop functions and Clear function to the pool

---
+++ SimplePool.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using Object = UnityEngine.Object;
+
+// Generic pool class
+// Author: Suleyman Yasir Kula
+// Feel free to use/upgrade
+public class SimplePool<T> where T : class
+{
+	// Objects stored in the pool
+	private List<T> pool = null;
+
+	// Blueprint to use for instantiation
+	private T m_blueprint;
+	private Object m_blueprintUnityObject;
+
+	public T Blueprint {
+		get
+		{
+			return m_blueprint;
+		}
+		set
+		{
+			m_blueprint = value;
+			m_blueprintUnityObject = value as Object;
+		}
+	}
+
+	// A function that can be used to override default NewObject( T ) function
+	public Func<T, T> CreateFunction;
+
+	// Actions that can be used to implement extra logic on pushed/popped objects
+	public Action<T> OnPush, OnPop;
+
+	public SimplePool( Func<T, T> CreateFunction = null, Action<T> OnPush = null, Action<T> OnPop = null )
+	{
+		pool = new List<T>();
+
+		this.CreateFunction = CreateFunction;
+		this.OnPush = OnPush;
+		this.OnPop = OnPop;
+	}
+
+	public SimplePool( T blueprint, Func<T, T> CreateFunction = null, Action<T> OnPush = null, Action<T> OnPop = null )
+							: this( CreateFunction, OnPush, OnPop )
+	{
+		// Set the blueprint at creation
+		Blueprint = blueprint;
+	}
+
+	// Populate the pool with the default blueprint
+	public bool Populate( int count )
+	{
+		return Populate( Blueprint, count );
+	}
+
+	// Populate the pool with a specific blueprint
+	public bool Populate( T blueprint, int count )
+	{
+		if( count <= 0 )
+			return true;
+
+		// Create a single object first to see if everything works fine
+		// If not, return false
+		T obj = NewObject( blueprint );
+		if( obj == null )
+			return false;
+
+		Push( obj );
+
+		// Everything works fine, populate the pool with the remaining items
+		for( int i = 1; i < count; i++ )
+		{
+			Push( NewObject( blueprint ) );
+		}
+
+		return true;
+	}
+
+	// Fetch an item from the pool
+	public T Pop()
+	{
+		T objToPop;
+
+		if( pool.Count == 0 )
+		{
+			// Pool is empty, instantiate the blueprint
+
+			objToPop = NewObject( Blueprint );
+		}
+		else
+		{
+			// Pool is not empty, fetch the first item in the pool
+
+			int index = pool.Count - 1;
+			objToPop = pool[index];
+			pool.RemoveAt( index );
+		}
+
+		if( OnPop != null )
+			OnPop( objToPop );
+
+		return objToPop;
+	}
+
+	// Fetch multiple items at once from the pool
+	public T[] Pop( int count )
+	{
+		if( count <= 0 )
+			return new T[0];
+
+		T[] result = new T[count];
+		for( int i = 0; i < count; i++ )
+			result[i] = Pop();
+
+		return result;
+	}
+
+	// Pool an item
+	public void Push( T obj )
+	{
+		if( obj == null ) return;
+
+		if( OnPush != null )
+			OnPush( obj );
+
+		pool.Add( obj );
+	}
+
+	// Pool multiple items at once
+	public void Push( IEnumerable<T> objects )
+	{
+		if( objects == null ) return;
+
+		foreach( T obj in objects )
+			Push( obj );
+	}
+
+	// Clear the pool (either completely or only the null entries, if any)
+	public void Clear( bool removeNullEntriesOnly = false, bool destroyRemovedObjects = true )
+	{
+		if( !removeNullEntriesOnly )
+		{
+			// Destroy the removed Objects
+			if( destroyRemovedObjects )
+				pool.ForEach( ( item ) => Object.Destroy( item as Object ) );
+
+			pool.Clear();
+		}
+		else
+			pool.RemoveAll( ( item ) => item == null );
+	}
+
+	// Create an instance of the blueprint and return it
+	private T NewObject( T blueprint )
+	{
+		if( CreateFunction != null )
+			return CreateFunction( blueprint );
+
+		if( blueprint == null || m_blueprintUnityObject == null )
+			return null;
+
+		return Object.Instantiate( m_blueprintUnityObject ) as T;
+	}
+}
---
+++ SimplePoolUtils.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+
+public static class SimplePoolHelper
+{
+	// Pool container for given type
+	private static class PoolsOfType<T> where T : class
+	{
+		// Pool with poolName = null
+		private static SimplePool<T> defaultPool = null;
+
+		// Other pools
+		private static Dictionary<string, SimplePool<T>> namedPools = null;
+
+		public static SimplePool<T> GetPool( string poolName = null )
+		{
+			if( poolName == null )
+			{
+				if( defaultPool == null )
+					defaultPool = new SimplePool<T>();
+
+				return defaultPool;
+			}
+			else
+			{
+				SimplePool<T> result;
+
+				if( namedPools == null )
+				{
+					namedPools = new Dictionary<string, SimplePool<T>>();
+
+					result = new SimplePool<T>();
+					namedPools.Add( poolName, result );
+				}
+				else if( !namedPools.TryGetValue( poolName, out result ) )
+				{
+					result = new SimplePool<T>();
+					namedPools.Add( poolName, result );
+				}
+
+				return result;
+			}
+		} 
+	}
+
+	// NOTE: if you don't need two or more pools of same type,
+	// leave poolName as null while calling any of these functions
+	// for better performance
+
+	public static SimplePool<T> GetPool<T>( string poolName = null ) where T : class
+	{
+		return PoolsOfType<T>.GetPool( poolName );
+	}
+
+	public static void Push<T>( T obj, string poolName = null ) where T : class
+	{
+		PoolsOfType<T>.GetPool( poolName ).Push( obj );
+	}
+
+	public static T Pop<T>( string poolName = null ) where T : class
+	{
+		return PoolsOfType<T>.GetPool( poolName ).Pop();
+	}
+
+	// Extension method as a shorthand for Push function
+	public static void Pool<T>( this T obj, string poolName = null ) where T : class
+	{
+		PoolsOfType<T>.GetPool( poolName ).Push( obj );
+	}
+}
